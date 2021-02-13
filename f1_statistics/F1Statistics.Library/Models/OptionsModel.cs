12e248af0b1a35c4709769a099761d3e625e546d
Tomas Kaselynas <tomas.kaselynas@visma.com>
2021-02-13 13:56:22 +0200

    Create OptionsModel

---
+++ f1_statistics/F1Statistics.Library/Models/OptionsModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace F1Statistics.Library.Models
+{
+    public class OptionsModel
+    {
+        public int YearFrom { get; set; }
+        public int YearTo { get; set; }
+        public int Season { get; set; }
+    }
+}
