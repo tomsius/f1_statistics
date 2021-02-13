59d6bf6d03f9fd20897e567ec5f8c7de126f8dc9
Tomas Kaselynas <tomas.kaselynas@visma.com>
2021-02-13 15:48:30 +0200

    Create model to store winner and amount of wins

---
+++ f1_statistics/F1Statistics.Library/Models/WinsModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace F1Statistics.Library.Models
+{
+    public class WinsModel
+    {
+        public string Name { get; set; }
+        public int WinCount { get; set; }
+    }
+}
